3c114d6cc5a0d13b85ffc10908aa190ee19e6c4d
gray-1to <graystin88@gmail.com>
2024-11-02 13:34:30 +0900

    matching/post実装

--- lib/gikusai-2024-10-29-stack.ts
+++ lib/gikusai-2024-10-29-stack.ts
@@ -2,7 +2,12 @@ import * as cdk from "aws-cdk-lib";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as apigateway from "aws-cdk-lib/aws-apigateway";
 import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
-import { Effect, PolicyStatement, Role, ServicePrincipal } from "aws-cdk-lib/aws-iam";
+import {
+  Effect,
+  PolicyStatement,
+  Role,
+  ServicePrincipal,
+} from "aws-cdk-lib/aws-iam";
 import { Construct } from "constructs";
 import * as path from "path";
 
@@ -63,28 +68,52 @@ export class Gikusai20241029Stack extends cdk.Stack {
     });
 
     // matching/list
-    const MatchingListLambda = new lambda.Function(this, "MatchingListLambdaFunction", {
-      runtime: lambda.Runtime.PYTHON_3_11,
-      handler: "list.lambda_handler",
-      code: lambda.Code.fromAsset(path.join(__dirname, "../lambda/matching")),
-      role: lambdaExecutionRole,
-      timeout: cdk.Duration.seconds(30),
-      environment: {
-        TABLE_NAME: table.tableName, // 環境変数にDynamoDBテーブル名を設定
-      },
-    });
+    const MatchingListLambda = new lambda.Function(
+      this,
+      "MatchingListLambdaFunction",
+      {
+        runtime: lambda.Runtime.PYTHON_3_11,
+        handler: "list.lambda_handler",
+        code: lambda.Code.fromAsset(path.join(__dirname, "../lambda/matching")),
+        role: lambdaExecutionRole,
+        timeout: cdk.Duration.seconds(30),
+        environment: {
+          TABLE_NAME: table.tableName, // 環境変数にDynamoDBテーブル名を設定
+        },
+      }
+    );
 
     // matching/list
-    const MatchingMatchLambda = new lambda.Function(this, "MatchingMatchLambdaFunction", {
-      runtime: lambda.Runtime.PYTHON_3_11,
-      handler: "match.lambda_handler",
-      code: lambda.Code.fromAsset(path.join(__dirname, "../lambda/matching")),
-      role: lambdaExecutionRole,
-      timeout: cdk.Duration.seconds(30),
-      environment: {
-        TABLE_NAME: table.tableName, // 環境変数にDynamoDBテーブル名を設定
-      },
-    });
+    const MatchingMatchLambda = new lambda.Function(
+      this,
+      "MatchingMatchLambdaFunction",
+      {
+        runtime: lambda.Runtime.PYTHON_3_11,
+        handler: "match.lambda_handler",
+        code: lambda.Code.fromAsset(path.join(__dirname, "../lambda/matching")),
+        role: lambdaExecutionRole,
+        timeout: cdk.Duration.seconds(30),
+        environment: {
+          TABLE_NAME: table.tableName, // 環境変数にDynamoDBテーブル名を設定
+        },
+      }
+    );
+
+    // matching/list
+    const MatchingPostLambda = new lambda.Function(
+      this,
+      "MatchingPostLambdaFunction",
+      {
+        runtime: lambda.Runtime.PYTHON_3_11,
+        handler: "post.lambda_handler",
+        code: lambda.Code.fromAsset(path.join(__dirname, "../lambda/matching")),
+        role: lambdaExecutionRole,
+        timeout: cdk.Duration.seconds(30),
+        environment: {
+          TABLE_NAME: table.tableName, // 環境変数にDynamoDBテーブル名を設定
+        },
+      }
+    );
 
     // API Gatewayを作成してLambda関数を統合
     const api = new apigateway.LambdaRestApi(this, "HelloLambdaApi", {
@@ -94,74 +123,146 @@ export class Gikusai20241029Stack extends cdk.Stack {
 
     // リソースとメソッドを追加
     const helloResource = api.root.addResource("hello");
-    helloResource.addMethod("GET"); 
-    helloResource.addMethod("OPTIONS", new apigateway.MockIntegration({
-      integrationResponses: [{
-        statusCode: "200",
-        responseParameters: {
-          "method.response.header.Access-Control-Allow-Origin": "'*'",
-          "method.response.header.Access-Control-Allow-Methods": "'OPTIONS,GET'",
-          "method.response.header.Access-Control-Allow-Headers": "'Content-Type'",
-        },
-      }],
-      passthroughBehavior: apigateway.PassthroughBehavior.NEVER,
-    }), {
-      methodResponses: [{
-        statusCode: "200",
-        responseParameters: {
-          "method.response.header.Access-Control-Allow-Origin": true,
-          "method.response.header.Access-Control-Allow-Methods": true,
-          "method.response.header.Access-Control-Allow-Headers": true,
-        },
-      }],
-    });
+    helloResource.addMethod("GET");
+    helloResource.addMethod(
+      "OPTIONS",
+      new apigateway.MockIntegration({
+        integrationResponses: [
+          {
+            statusCode: "200",
+            responseParameters: {
+              "method.response.header.Access-Control-Allow-Origin": "'*'",
+              "method.response.header.Access-Control-Allow-Methods":
+                "'OPTIONS,GET'",
+              "method.response.header.Access-Control-Allow-Headers":
+                "'Content-Type'",
+            },
+          },
+        ],
+        passthroughBehavior: apigateway.PassthroughBehavior.NEVER,
+      }),
+      {
+        methodResponses: [
+          {
+            statusCode: "200",
+            responseParameters: {
+              "method.response.header.Access-Control-Allow-Origin": true,
+              "method.response.header.Access-Control-Allow-Methods": true,
+              "method.response.header.Access-Control-Allow-Headers": true,
+            },
+          },
+        ],
+      }
+    );
 
     const matchingResource = api.root.addResource("matching");
     const matchingListResource = matchingResource.addResource("list");
-    matchingListResource.addMethod("GET", new apigateway.LambdaIntegration(MatchingListLambda)); // POSTメソッドを追加
-    matchingListResource.addMethod("OPTIONS", new apigateway.MockIntegration({
-      integrationResponses: [{
-        statusCode: "200",
-        responseParameters: {
-          "method.response.header.Access-Control-Allow-Origin": "'*'",
-          "method.response.header.Access-Control-Allow-Methods": "'OPTIONS,GET'",
-          "method.response.header.Access-Control-Allow-Headers": "'Content-Type'",
-        },
-      }],
-      passthroughBehavior: apigateway.PassthroughBehavior.NEVER,
-    }), {
-      methodResponses: [{
-        statusCode: "200",
-        responseParameters: {
-          "method.response.header.Access-Control-Allow-Origin": true,
-          "method.response.header.Access-Control-Allow-Methods": true,
-          "method.response.header.Access-Control-Allow-Headers": true,
-        },
-      }],
-    });
-    
+    matchingListResource.addMethod(
+      "GET",
+      new apigateway.LambdaIntegration(MatchingListLambda)
+    ); // POSTメソッドを追加
+    matchingListResource.addMethod(
+      "OPTIONS",
+      new apigateway.MockIntegration({
+        integrationResponses: [
+          {
+            statusCode: "200",
+            responseParameters: {
+              "method.response.header.Access-Control-Allow-Origin": "'*'",
+              "method.response.header.Access-Control-Allow-Methods":
+                "'OPTIONS,GET'",
+              "method.response.header.Access-Control-Allow-Headers":
+                "'Content-Type'",
+            },
+          },
+        ],
+        passthroughBehavior: apigateway.PassthroughBehavior.NEVER,
+      }),
+      {
+        methodResponses: [
+          {
+            statusCode: "200",
+            responseParameters: {
+              "method.response.header.Access-Control-Allow-Origin": true,
+              "method.response.header.Access-Control-Allow-Methods": true,
+              "method.response.header.Access-Control-Allow-Headers": true,
+            },
+          },
+        ],
+      }
+    );
+
     const matchingMatchResource = matchingResource.addResource("match");
-    matchingMatchResource.addMethod("GET", new apigateway.LambdaIntegration(MatchingMatchLambda)); // POSTメソッドを追加
-    matchingMatchResource.addMethod("OPTIONS", new apigateway.MockIntegration({
-      integrationResponses: [{
-        statusCode: "200",
-        responseParameters: {
-          "method.response.header.Access-Control-Allow-Origin": "'*'",
-          "method.response.header.Access-Control-Allow-Methods": "'OPTIONS,GET'",
-          "method.response.header.Access-Control-Allow-Headers": "'Content-Type'",
-        },
-      }],
-      passthroughBehavior: apigateway.PassthroughBehavior.NEVER,
-    }), {
-      methodResponses: [{
-        statusCode: "200",
-        responseParameters: {
-          "method.response.header.Access-Control-Allow-Origin": true,
-          "method.response.header.Access-Control-Allow-Methods": true,
-          "method.response.header.Access-Control-Allow-Headers": true,
-        },
-      }],
-    });
+    matchingMatchResource.addMethod(
+      "GET",
+      new apigateway.LambdaIntegration(MatchingMatchLambda)
+    ); // POSTメソッドを追加
+    matchingMatchResource.addMethod(
+      "OPTIONS",
+      new apigateway.MockIntegration({
+        integrationResponses: [
+          {
+            statusCode: "200",
+            responseParameters: {
+              "method.response.header.Access-Control-Allow-Origin": "'*'",
+              "method.response.header.Access-Control-Allow-Methods":
+                "'OPTIONS,GET'",
+              "method.response.header.Access-Control-Allow-Headers":
+                "'Content-Type'",
+            },
+          },
+        ],
+        passthroughBehavior: apigateway.PassthroughBehavior.NEVER,
+      }),
+      {
+        methodResponses: [
+          {
+            statusCode: "200",
+            responseParameters: {
+              "method.response.header.Access-Control-Allow-Origin": true,
+              "method.response.header.Access-Control-Allow-Methods": true,
+              "method.response.header.Access-Control-Allow-Headers": true,
+            },
+          },
+        ],
+      }
+    );
+
+    const matchingPostResource = matchingResource.addResource("post");
+    matchingPostResource.addMethod(
+      "POST",
+      new apigateway.LambdaIntegration(MatchingPostLambda)
+    );
+    matchingPostResource.addMethod(
+      "OPTIONS",
+      new apigateway.MockIntegration({
+        integrationResponses: [
+          {
+            statusCode: "200",
+            responseParameters: {
+              "method.response.header.Access-Control-Allow-Origin": "'*'",
+              "method.response.header.Access-Control-Allow-Methods":
+                "'OPTIONS,GET'",
+              "method.response.header.Access-Control-Allow-Headers":
+                "'Content-Type'",
+            },
+          },
+        ],
+        passthroughBehavior: apigateway.PassthroughBehavior.NEVER,
+      }),
+      {
+        methodResponses: [
+          {
+            statusCode: "200",
+            responseParameters: {
+              "method.response.header.Access-Control-Allow-Origin": true,
+              "method.response.header.Access-Control-Allow-Methods": true,
+              "method.response.header.Access-Control-Allow-Headers": true,
+            },
+          },
+        ],
+      }
+    );
 
     // APIエンドポイントURLを出力
     new cdk.CfnOutput(this, "ApiUrl", {
